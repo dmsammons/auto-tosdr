9c0f9574c8b74e87e4fb5a48dab819828d173c00
Daniel Sammons <danielsammons@gmail.com>
2016-12-04 12:55:26 -0800

    Fix flake8 output

--- scripts/txt2json.py
+++ scripts/txt2json.py
@@ -8,13 +8,13 @@ import os
 def main(args):
     basename = os.path.basename(args.textfile)
     name, _ = os.path.splitext(basename)
-    
+
     with open(args.textfile, 'r') as f:
         txt = f.read()
-    
+
     data = {'label': args.rating, 'text': txt}
     data['url'] = args.url if args.url else ''
-    
+
     with open('{}.{}'.format(name, 'json'), 'w') as f:
         json.dump(data, f, indent=4)
 
@@ -24,8 +24,8 @@ def cli():
     parser.add_argument("textfile", type=str, help="File to containing TOS.")
     parser.add_argument("--rating", type=str, help="TOSDR rating associated "
                         "with the TOS text. Options={'A', 'B', 'C', 'D', E'}")
-    parser.add_argument("--url", type=str, help="URL to the webpage containing "
-                        "the TOS.")
+    parser.add_argument("--url", type=str, help="URL to the webpage "
+                        "containing the TOS.")
     return parser.parse_args()
 
 if __name__ == '__main__':
